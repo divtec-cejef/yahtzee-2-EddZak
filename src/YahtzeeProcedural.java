cebd2c73238d346c987eafa4f217edb7a4e3da5b
eddizak <zakaria.eddini@divtec.ch>
2025-08-20 09:37:13 +0200

    feat: Premier Jalon de l'exercice Yahtzee

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,34 @@
+/*
+ * Auteur: Zakaria Eddini
+ * Date: 20.08.2025
+ * But: Jeu Yahtzee en Java
+ */
+
+
 public class YahtzeeProcedural {
 
-    public static void main(String[] args) {
-        System.out.println( "Bon courage !");
+    // Lance un dé
+    public static int lancerDe() {
+        return (int)(Math.random() * 6 + 1);
+    }
 
+    // Affiche les résultats des dés
+    public static void afficherDes(int[] resultatDes) {
+        for (int i = 0; i < resultatDes.length; i++) {
+            System.out.println("Résultat du " + (i+1) + " dé est : " + resultatDes[i]);
+        }
+    }
+
+    // Fonction principale
+    public static int[] lancerTousLesDes(int nombreDeDes) {
+        int[] resultat = new int[nombreDeDes];
+        for (int i = 0; i < nombreDeDes; i++) {
+            resultat[i] = lancerDe();
+        }
+        return resultat;
+    }
+    public static void main(String[] args) {
+        int[] resultatDes = lancerTousLesDes(5);
+        afficherDes(resultatDes);
     }
 }
